73e38a7b9e11d5447d0d0fb70e666c33534f7d29
othmane099 <othmaben99@gmail.com>
2022-02-07 09:07:02 +0100

    add ForgotActivity

---
+++ app/src/main/java/com/odev/thesis/ForgotPasswordActivity.java
@@ -0,0 +1,88 @@
+package com.odev.thesis;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.odev.thesis.databinding.ActivityForgotPasswordBinding;
+
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private ActivityForgotPasswordBinding binding;
+
+    private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityForgotPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait...");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        binding.submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateData();
+            }
+        });
+    }
+
+    private String email = "";
+    private void validateData() {
+        email = binding.emailEt.getText().toString().trim();
+
+        if (email.isEmpty()){
+            Toast.makeText(this, "Enter email...", Toast.LENGTH_SHORT).show();
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this, "Invalid email format...", Toast.LENGTH_SHORT).show();
+        }else{
+            recoverPassword();
+        }
+    }
+
+    private void recoverPassword() {
+        progressDialog.setMessage("Sending password recovery instructions to "+email);
+        progressDialog.show();
+
+        firebaseAuth.sendPasswordResetEmail(email)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ForgotPasswordActivity.this, "Instructions to reset password sent to "+email, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ForgotPasswordActivity.this, "Failed to send due to "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+}
