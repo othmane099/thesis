e2c2c6d27661482ea26b4cd6a9086a17ce0971c2
othmane099 <othmaben99@gmail.com>
2022-02-07 09:02:58 +0100

    add checkUser method to SplashActivity

--- app/src/main/java/com/odev/thesis/SplashActivity.java
+++ app/src/main/java/com/odev/thesis/SplashActivity.java
@@ -1,26 +1,78 @@
 package com.odev.thesis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class SplashActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
         // start main screen after two seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+                checkUser();
             }
         }, 2000);
+    }
+
+    private void checkUser(){
+        // get current user, if logged in
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null){
+            //user not logged in
+            //start main screen
+            // start main screen
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }else {
+            DatabaseReference ref = FirebaseDatabase
+                    .getInstance()
+                    .getReference("Users");
+            ref.child(firebaseUser.getUid())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //get user type
+                            String userType = ""+snapshot.child("userType").getValue();
+                            //check user type
+                            if (userType.equals("user")){
+                                //this is simple user, open user dashboard
+                                startActivity(new Intent(SplashActivity.this, UserDashboardActivity.class));
+                                finish();
+                            }else if (userType.equals("admin")){
+                                //this is admin, open admin dashboard
+                                startActivity(new Intent(SplashActivity.this, AdminDashboardActivity.class));
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+        }
     }
 }
